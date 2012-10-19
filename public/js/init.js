200b664a83d8a6bdb27314addc7e0a2d47fb4c97
Matthew Frey <matthewfrey118@gmail.com>
2012-10-19 17:02:32 -0400

    Layout mostly complete

---
+++ public/js/init.js
@@ -0,0 +1,45 @@
+$(function() 
+{
+	$('h1').hide().fadeIn(1500);
+	updateRows();
+});
+
+//globals
+var rows = new Array();
+
+//start or stop a row
+function toggle(index)
+{
+	alert("clicked" + index);
+}
+
+//add a timer row
+function addRow()
+{
+	rows.push(0);
+}
+
+//delete a timer row
+function removeRow(index)
+{
+	$('.row').each( function(index) {
+		$(this).fadeOut(1000).remove();
+	});
+}
+
+
+//Set each row's button's
+function updateRows()
+{
+	$('.row').each( function(index, element){
+		$('a.del').click( function() 
+		{
+			alert('trying');
+			removeRow(index);
+		});
+		
+		$('p.time').click( function()
+		{
+			toggle(index);
+		});
+	});
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'ttrack' });
+  res.render('index', { title: 'ttrack.js' });
 };
