7badaac6b03beba5a6a10156adbfc1e2f31d35f5
Matthew Frey <matthewfrey118@gmail.com>
2012-10-26 14:06:12 -0400

    routed / to [missing] index.jade. routed /ttrack to ttrack.jade. Branch is for eventual personal site.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
+  , ttrack = require('./routes/ttrack')
   , http = require('http')
   , path = require('path');
 
@@ -28,7 +28,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/ttrack', ttrack.index);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
